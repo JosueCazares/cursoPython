5558a3e29005aca2f268c98f7a335c7999575f06
JosueCazares <josuecazares2004@gmail.com>
2023-09-03 13:20:20 -0600

    sentencias print

---
+++ pruebas/practicas.py
@@ -0,0 +1,17 @@
+import sys
+#version de py
+"""
+print (sys.version)
+
+print(sys.version_info)
+"""
+#saltos de linea 
+print("ejemplo", end="")
+print("ejemplo") # el end hizo que se concatenaran las lineas, no salto de linea  
+print("ejemplo", "de" ,"separeitor(sep)", sep="*") #hace que en el espacio de lineas introdusca el caracquer que le indiques
+#place holder
+print("{} ejemplo {}".format("arroz","potro"))
+#arreglo
+print()
+numeros=[1,2,4,5,9]
+print(numeros)
