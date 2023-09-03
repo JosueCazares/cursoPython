91596d16e2eace444bd2b94d64618940afc0e185
JosueCazares <josuecazares2004@gmail.com>
2023-09-03 13:40:10 -0600

    area de circulo

---
+++ pruebas/areaCirculo.py
@@ -0,0 +1,6 @@
+#solicitar datos para calcular area de circulo
+
+print("Dame el radio del cirulo")
+radio = float(input())
+area=3.1416 * radio ** 2
+print("El area del circulo es {}".format(area))   
